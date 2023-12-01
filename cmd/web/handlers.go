ef600ce76c008ef6c779f447ef93fd81a16d203e
Usif Ahmed <usifahmedmuhammed@gmail.com>
2023-12-01 15:02:00 +0200

    Testing & Context Optimization

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -147,3 +147,7 @@ func (app *applicatoin) logoutUser(w http.ResponseWriter, r *http.Request) {
 	app.session.Put(r, "flash", "You have been logged out successfully")
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
+
+func ping(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("OK"))
+}
